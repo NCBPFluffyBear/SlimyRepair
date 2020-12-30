d0f6a28815c24a37ba1c672a67bb0198adb326d1
NCBPFluffyBear <31554056+ncbpfluffybear@users.noreply.github.com>
2020-12-29 22:39:16 -0600

    Added bug tracker url because dis thing no build >:( ree

--- src/main/java/me/ncbpfluffybear/slimyrepair/SlimyRepair.java
+++ src/main/java/me/ncbpfluffybear/slimyrepair/SlimyRepair.java
@@ -80,8 +80,7 @@ public class SlimyRepair extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public String getBugTrackerURL() {
-        // You can return a link to your Bug Tracker instead of null here
-        return null;
+        return "https://github.com/NCBPFluffyBear/SlimyRepair/issues";
     }
 
     @Override
